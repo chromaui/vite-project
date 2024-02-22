7b64d63b9990d87fc9663139a00387fc1e5550ce
Reuben Ellis <ethriel3695@gmail.com>
2024-02-22 14:50:17 -0700

    Update stories

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -1,25 +1,33 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
+import { within, fn, expect, createEvent, fireEvent } from '@storybook/test';
 import { Header } from './Header';
 
-const meta: Meta<typeof Header> = {
+const meta = {
   title: 'Example/Header',
   component: Header,
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
   },
-};
+  args: {
+    onLogin: fn(),
+  },
+} satisfies Meta<typeof Header>;
 
 export default meta;
 
-type Story = StoryObj<typeof Header>;
+type Story = StoryObj<typeof meta>;
 
 export const LoggedIn: Story = {
-  args: {
-    user: {
-      name: 'Jane Doe',
-    },
+  args: {},
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const loginButton = await canvas.findByRole('button', { name: /Log in/i });
+    await expect(loginButton).toBeInTheDocument();
+    const clickHyperlink = createEvent.click(loginButton);
+    const isPrevented = fireEvent(loginButton, clickHyperlink);
+    await expect(args.onLogin).toHaveBeenCalled();
+    expect(isPrevented).toBe(true);
   },
 };
 
--- src/stories/Page.stories.tsx
+++ src/stories/Page.stories.tsx
@@ -1,26 +1,51 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { within, userEvent } from '@storybook/test';
+import { within, userEvent, fn, expect } from '@storybook/test';
 import { Page } from './Page';
 
-const meta: Meta<typeof Page> = {
+const meta = {
   title: 'Example/Page',
   component: Page,
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
   },
-};
+  args: {
+    onClick: fn(),
+    onLogin: fn(),
+  },
+} satisfies Meta<typeof Page>;
 
 export default meta;
 
-type Story = StoryObj<typeof Page>;
-export const LoggedOut: Story = {};
+type Story = StoryObj<typeof meta>;
+// export const LoggedOut: Story = {};
 
-export const LoggedIn: Story = {};
+export const LoggedIn: Story = {
+  args: {
+    user: { name: 'Test User' },
+  },
+  // play: async ({ args, canvasElement }) => {
+  //   const canvas = within(canvasElement);
+  //   const loginButton = await canvas.findByRole('button', { name: /Log in/i });
+  //   await expect(loginButton).toBeInTheDocument();
 
-// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
-LoggedIn.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
-  await userEvent.click(loginButton);
+  //   // const mockEvent = { preventDefault: fn() };
+  //   userEvent.click(loginButton);
+  //   await expect(args.onLogin).toHaveBeenCalled();
+  //   // expect(mockEvent.preventDefault).toBeCalled();
+  //   // const clickHyperlink = createEvent.click(loginButton);
+  //   // const isPrevented = fireEvent(loginButton, clickHyperlink);
+  //   // expect(clickHyperlink.defaultPrevented).toBe(true);
+  //   // expect(isPrevented).toBe(false);
+  //   // await userEvent.click(clickHyperlink);
+  // },
 };
+
+// // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
+// LoggedIn.play = async ({ canvasElement }) => {
+//   const canvas = within(canvasElement);
+//   const loginButton = await canvas.getByRole('button', { name: /Log out/i });
+//   const clickHyperlink = createEvent.click(loginButton);
+//   fireEvent(loginButton, clickHyperlink);
+//   // await userEvent.click(clickHyperlink);
+// };
