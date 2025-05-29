4477c693c83012c957f9733dc8498197778273a5
ethriel3695 <ethriel3695@gmail.com>
2025-05-28 20:01:07 -0600

    update comment in preview for a11y

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -24,11 +24,12 @@ const preview: Preview = {
     tags: ['autodocs'],
 
     a11y: {
+      // I didn't know this was a thing!
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
-      test: 'todo'
-    }
+      test: 'todo',
+    },
   },
   initialGlobals: {
     viewport: { value: 'iphone14', isRotated: false },
