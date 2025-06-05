9bd30c2016f5cc82f3aea5bba09655f356739880
ethriel3695 <ethriel3695@gmail.com>
2025-06-05 05:35:42 -0600

    remove comment in story

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -6,7 +6,6 @@ const meta = {
   title: 'Example/Header',
   component: Header,
   parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
   },
   args: {
